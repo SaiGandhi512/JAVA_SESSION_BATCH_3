8260be8f350fe4d5a1d591a9df9104ba7a1398e1
SaiGandhi512 <saigandhipaleru@gmail.com>
2025-01-07 10:34:35 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Long.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Double.SIZE);
+        System.out.println(Float.SIZE);
+        int num = Integer.MIN_VALUE;
+        System.out.println(num);
+    }
+}
