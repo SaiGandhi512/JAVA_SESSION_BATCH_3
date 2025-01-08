17cdee4817aa02aeae7ae174f49702d819b7d15e
SaiGandhi512 <saigandhipaleru@gmail.com>
2025-01-08 14:16:31 +0530

    Add files via upload

---
+++ src/Day_6_Problems.java
@@ -0,0 +1,14 @@
+public class Day_6_Problems{
+    public static void main(String[] args){
+        //nested loop
+        //outer loop for rows
+        for(int i=0;i<4;i++){
+            //inner loop for columns
+            for(int j=0;j<=i;j++){ // j = 0 < 4
+                System.out.print("y");
+
+            }
+            System.out.println();
+        }
+    }
+}
